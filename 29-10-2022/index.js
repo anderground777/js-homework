04d8d36490547654b89ff6b12b7045afff2d1b56
anderground777 <anderground@inbox.ru>
2022-11-11 15:48:29 +0300

    Перенес домашку от 29-10 в этот каталог

---
+++ 29-10-2022/index.js
@@ -0,0 +1,9 @@
+console.log('Hello, World!')
+let numberOf = 123;
+const bigestInt = 1234567891234567891234567890n;  
+let nameUser = 'Ilia';
+let isCash = true;
+let age = null;
+bigestInt = 10n;
+console.log(numberOf, bigestInt, nameUser, isCash, age);
+console.log(typeof age);
