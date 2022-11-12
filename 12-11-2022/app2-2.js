b10afdcab6405e8932d054783abafe0f3a99b20d
anderground777 <anderground@inbox.ru>
2022-11-12 13:33:45 +0300

    разобрали на уроке две задачи

--- 12-11-2022/app2-2.js
+++ 12-11-2022/app2-2.js
@@ -2,13 +2,36 @@
 // раза. Выяснить, на какой день продаж, количество доступных к покупке авто
 // станет меньше M, если известно, что в первый день продаж всего было N 
 // автомобилей.
+// делал дома
+// let N = 300;
+// let M = 50;
+// count = 1;
 
-let N = 300;
-let M = 50;
-count = 0;
+// while(N > M) {
+//     N = N/2;
+//     count++;
+// }
+// console.log(count);
 
-while(N > M) {
-    N = N/2;
-    count++;
+// разбор в классе через while
+const startCarCount = 1000;
+const minCarCount = 10;
+let day = 1;
+let currentCarCount = startCarCount;
+
+while (currentCarCount > minCarCount) {
+    currentCarCount = Math.round(currentCarCount / 2);
+    day++;
 }
-console.log(count);
+console.log(`Через ${day} дней.`)
+
+// //через for
+// const startCarCount = 1000;
+// const minCarCount = 10;
+// let dayCarMinCount = null;
+// let currentCarCount = startCarCount;
+// for (let day = 1; minCarCount < currentCarCount; index++) {
+//     currentCarCount = currentCarCount / 2;
+//     dayCarMinCount = day;
+// }
+// console.log(`Через ${day} дней.`)
